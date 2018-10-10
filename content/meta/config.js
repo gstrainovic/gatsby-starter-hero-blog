da756519e3ef0769a29f3f6373f1f1a0171d47d6
gstrainovic <g.strainovic@gmail.com>
2018-10-11 01:27:54 +0200

    insert gravatar

--- content/meta/config.js
+++ content/meta/config.js
@@ -19,6 +19,8 @@ module.exports = {
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
   manifestDisplay: "standalone",
+  // gravatar
+  gravatarMD5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
   // social
   authorSocialLinks: [
     { name: "github", url: "https://github.com/greglobinski" },
